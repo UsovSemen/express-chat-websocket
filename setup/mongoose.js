983e7a41e74709d761b0aeae4cd306b7e8c146f9
Sam <usov.semen@gmail.com>
2016-12-08 17:35:01 +0300

    model

---
+++ setup/mongoose.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sam on 28.11.2016.
+ */
+var mongoose = require('mongoose');
+var config = require('../config');
+
+mongoose.connect(config.get("mongoose:uri"), config.get('mongoose:options'));
+
+module.exports = mongoose;
